d11e0cb0de128f261a949b72ea220cced1c7a944
agutuomondi <agutudennis1997@gmail.com>
2024-07-04 19:40:15 -0700

    3-to_str.py

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
-
-""" Module that contains type-annotated function to_str that takes a float n
-as an argument and returns the string representation of the float.
-"""
+'''Module for task 3.
+'''
 
 
 def to_str(n: float) -> str:
-    """Function to return the string representation of a float.
-    """
-    return str(n
+    '''Cast a floting type number in to a string.
+    '''
+    return str(n)
